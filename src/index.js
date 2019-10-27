19a7d32976bc13b02375ae97720247d8603b65c7
nicaa0695 <nicaa0695@gmail.com>
2019-10-27 15:11:01 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('input[type="submit"]').addEventListener("click", function(event){
+    console.log("hey, I'm executing")
+    let text = document.getElementById('new-task-description').value;
+    document.getElementById('tasks').innerHTML += text;
+    event.preventDefault();
+    }, false);
 });
